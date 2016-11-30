213b42b0891e5f3d1862a492b4d77d1f6011a4e3
William Ivanov <wivanov.ra@mysummitps.org>
2016-11-30 19:27:06 +0000

    Added 2nd To-Do List

--- toDo.js
+++ toDo.js
@@ -1,7 +1,11 @@
+//create a variable for each element I will use in javascript
 var addElementBtn = document.getElementById("addElementBtn");
+var deleteAllBtn = document.getElementById("deleteAllBtn");
+var container2 = document.getElementById("container2");
 var toDoInput = document.getElementById("toDoInput");
 var list = document.getElementById("list");
 
+//this function makes each list element when the add element button is clicked
 addElementBtn.addEventListener("click", function(){
     
     //create a div
@@ -22,18 +26,27 @@ addElementBtn.addEventListener("click", function(){
     //put the p within the listItemDiv
     listItemDiv.appendChild(listItemText);
     
-    //create an image
+    //create an image for deleting the list item
     var listItemDeleteButton = document.createElement("img");
     //make the image be the right size and have the right image
     listItemDeleteButton.setAttribute("src","RedX.png");
     listItemDeleteButton.setAttribute("height","24px");
     listItemDeleteButton.setAttribute("width","24px");
     // this line didn't work:   listItemDeleteButton.setAttribute("float","right");
-    //so I used the next line instead:
+    //so I used the next line instead to make the image stay on he right side:
     listItemDeleteButton.setAttribute("style","float: right;");
     //put the image in the list item div, after the p
     listItemText.appendChild(listItemDeleteButton);
     
+    //when the delete all button is clicked
+    deleteAllBtn.addEventListener("click", function(){
+        //if the list has items
+        while(list.hasChildNodes())
+            //delete the items
+            list.removeChild(list.childNodes[0]);
+    })
+    
+    //
     listItemDeleteButton.addEventListener("click", function(){
         listItemDeleteButton.parentNode.parentNode.removeChild(listItemDeleteButton.parentNode);
     })
